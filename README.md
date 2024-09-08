cf378545610bc5e0a72dc88e7cdf37cc57501d66
srkarmakar <167770618+srkarmakar@users.noreply.github.com>
2024-09-08 09:51:43 +0530

    Two Sum

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## 1 Reverse a String
 ## 2 Simple Array Sort
 ## 3 Add odd indexed of 2 arrays and keep the sum inside a new array
+## Two Sum
+## Median of Two Sorted Arrays
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function reverseString(string) {
     console.log(reversedString);
 }
 
-reverseString('Write Anything It will Reverse');
+//reverseString('Write Anything It will Reverse');
 
 /*----------------------------------------------------------------------------------------------------------------------------------------*/
 // Simple Array Sort
@@ -18,7 +18,7 @@ function arraySort(arr) {
     return sortedArray
 }
 
-console.log(arraySort([656.7, 5454, 55, .38, 1231]));
+//console.log(arraySort([656.7, 5454, 55, .38, 1231]));
 
 /*----------------------------------------------------------------------------------------------------------------------------------------*/
 // Add odd indexed of 2 arrays and keep the sum inside a new array
@@ -41,6 +41,42 @@ function addOddIndexedElements(arr1, arr2) {
     return sumedArr;
 }
 
-console.log(addOddIndexedElements([7, 8, 9, 4, 5], [71, 8, 29, 4, 55, 3, 9, 7]));
+//console.log(addOddIndexedElements([7, 8, 9, 4, 5], [71, 8, 29, 4, 55, 3, 9, 7]));
 
 /*----------------------------------------------------------------------------------------------------------------------------------------*/
+// Two Sum
+
+var twoSum = function (nums, target) {
+    let result = [];
+    for (let i = 0; i <= nums.length - 1; i++) {
+        for (let j = i + 1; j <= nums.length - 1; j++) {
+            if (nums[i] + nums[j] == target) {
+                result.push(i, j);
+            }
+        }
+    }
+    return result
+};
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 4, 2], 6));
+// console.log(twoSum([3, 3], 6));
+// console.log(twoSum([3, 2, 3], 6));
+
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+// Median of Two Sorted Arrays
+var findMedianSortedArrays = function (nums1, nums2) {
+    let median = 0;
+    let merged = [...nums1, ...nums2];
+    merged.sort((a, b) => a - b);
+    if (merged.length % 2 != 0) {
+        median = merged[Math.trunc(merged.length / 2)];
+    } else {
+        median = (merged[Math.trunc(merged.length / 2)] + merged[Math.trunc(merged.length / 2 - 1)]) / 2
+    }
+    console.log(merged);
+    return median
+};
+
+// console.log(findMedianSortedArrays([1, 3], [2]));
+// console.log(findMedianSortedArrays([1, 3], [4, 2]));
+// console.log(findMedianSortedArrays([10, 7 , 14], [14, 2, 5 , 13]));
